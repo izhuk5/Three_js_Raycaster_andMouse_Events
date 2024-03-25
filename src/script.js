85a044bb38f328dddfb9e8dae054ed4175ff1c90
Ihor Zhuk <ihorzhuk55555@icloud.com>
2024-03-25 14:29:58 +0200

    test on each frame

--- src/script.js
+++ src/script.js
@@ -39,21 +39,9 @@ scene.add(object1, object2, object3);
 // Raycaster
 const raycaster = new THREE.Raycaster();
 
-const rayOrigin = new THREE.Vector3(-3, 0, 0);
-const rayDirection = new THREE.Vector3(10, 0, 0);
-rayDirection.normalize();
-
 object1.updateMatrix(0.5, 2.5, 4.5);
 object2.updateMatrix(0.5, 2.5, 4.5);
 object3.updateMatrix(0.5, 2.5, 4.5);
-
-raycaster.set(rayOrigin, rayDirection);
-
-const intersect = raycaster.intersectObject(object2);
-console.log(intersect);
-
-const intersects = raycaster.intersectObjects([object1, object2, object3]);
-console.log(intersects);
 
 /**
  * Sizes
@@ -110,6 +98,29 @@ const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+
+  // Animate objects
+  object1.position.y = Math.sin(elapsedTime * 0.3) * 1.5;
+  object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5;
+  object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5;
+
+  // Cast a ray
+  const rayOrigin = new THREE.Vector3(-3, 0, 0);
+  const rayDirection = new THREE.Vector3(1, 0, 0);
+  rayDirection.normalize();
+
+  raycaster.set(rayOrigin, rayDirection);
+
+  const objectsToTest = [object1, object2, object3];
+  const intersects = raycaster.intersectObjects(objectsToTest);
+
+  for (const object of objectsToTest) {
+    object.material.color.set("#ff0000");
+  }
+
+  for (const intersect of intersects) {
+    intersect.object.material.color.set("#0000ff");
+  }
 
   // Update controls
   controls.update();
